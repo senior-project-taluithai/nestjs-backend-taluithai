8ef52f58b336328b98b9ded0baec0ea315bfecc7
Wissarut Kanasub <wissarut.dev@gmail.com>
2026-01-04 12:44:31 +0700

    CORS integrated

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  app.enableCors({
+    origin: configService.get('FRONTEND_URL'),
+    credentials: true,
+  });
+
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
