cf6ee5ae0ab45810b3dcfb55c0f6a3f46b614b4b
Wissarut Kanasub <wissarut.dev@gmail.com>
2026-01-04 11:18:54 +0700

    format code for better quality

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -26,7 +26,7 @@ export class UsersService {
   async findByResetToken(token: string): Promise<User | null> {
     return this.usersRepository.findOne({ where: { resetToken: token } });
   }
-  
+
   async update(id: string, data: Partial<User>): Promise<void> {
     await this.usersRepository.update(id, data);
   }
